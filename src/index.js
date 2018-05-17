b56d23f720950c905946bc50f15fd45e6dc8dcc8
Makar Stetsenko <makazone@gmail.com>
2018-05-17 13:45:34 +0300

    step1 start

--- src/index.js
+++ src/index.js
@@ -1,51 +1,39 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require("graphql-yoga");
 
 const postData = [
   {
-    id: 'post-0',
-    title: '',
-    content: '',
-    published: false,
-  },
-]
+    id: "post-0",
+    title: "",
+    content: "",
+    published: false
+  }
+];
 
 const typeDefs = `
-type Post {
-  id: ID!
-  title: String!
-  content: String!
-  published: Boolean!
-}
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    published: Boolean!
+  }
 
-type Query {
-  info: String!
-  posts(searchString: String): [Post!]!
-  post(id: ID!): Post
-}
-`
+  type Query {
+    info: String!
+    posts: [Post!]!
+  }
+`;
 
 const resolvers = {
   Query: {
     info: () => `This is the API for a simple blogging application.`,
-    posts: (_, args) => {
-      return args.searchString
-        ? postData.filter(
-            post =>
-              post.title.includes(args.searchString) ||
-              post.content.includes(args.searchString),
-          )
-        : postData
-    },
-    post: (_, args) => {
-      return postData.find(post => post.id === args.id)
-    },
-  },
-}
+    posts: () => postData
+  }
+};
 
 const server = new GraphQLServer({
   typeDefs,
-  resolvers,
-})
+  resolvers
+});
 server.start(() =>
-  console.log(`GraphQL server is running on http://localhost:4000`),
-)
+  console.log(`GraphQL server is running on http://localhost:4000`)
+);
